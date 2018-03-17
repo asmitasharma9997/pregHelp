0f8dc5b8a9426aed2b3286d16da77c5e798a2add
papa <saisurbhi@gmail.com>
2018-03-17 05:54:49 +0530

    preghelp startercode , ui

--- app/src/main/java/com/example/lenovo/preghelp/Track_medicine.java
+++ app/src/main/java/com/example/lenovo/preghelp/Track_medicine.java
@@ -1,13 +1,31 @@
 package com.example.lenovo.preghelp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Track_medicine extends AppCompatActivity {
 
+    TextView tv1;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_medicine);
+
+        tv1=(TextView)findViewById(R.id.textView2);
+        b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
     }
 }
